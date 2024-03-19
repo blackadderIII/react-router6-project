f36f8dbcc2895dd247b3c3ff5aff13bb9b620ab4
blackadderIII <skobarnes1@gmail.com>
2024-03-19 19:07:26 +0000

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Link, Navigate, Route, Routes } from "react-router-dom";
+import { useState } from "react";
+
+//Pages
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Products from "./pages/Products";
+import ProductDetails from "./pages/ProductDetails";
 
 function App() {
+  const [cartisEmpty] = useState(true);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <nav>
+          <h1> The Fortress Of Solitude</h1>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/products">Products</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+
+          <Route path="/about/*" element={<About />} />
+
+          <Route path="/products/:id/*" element={<ProductDetails />} />
+
+          <Route path="/products" element={<Products />} />
+          <Route
+            path="/test"
+            element={
+              <div className="content">
+                <h1>This is a test.</h1>
+              </div>
+            }
+          />
+          <Route
+            path="/redirect"
+            element={
+              cartisEmpty ? (
+                <Navigate to={"/products"} />
+              ) : (
+                <h1>Failure useState is currently false</h1>
+              )
+            }
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
